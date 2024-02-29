c5a1bb58169bcca0070c5ebacbcd1c6b213fdbe8
AbhishekPatil313 <abhishekpatil3311@gmail.com>
2024-02-29 17:51:35 +0530

    added

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,7 +3,7 @@ import styles from "./blog.module.css"
 import PostCard from '@/components/postCard/postCard'
 import { getPosts } from '@/lib/data';
 const  getData = async ()=> {
-  const res = await fetch("http://localhost:3000/api/blog");
+  const res = await fetch("https://nextjs-blog-app-five-virid.vercel.app/api/blog");
   if(!res.ok){
     throw new Error("Something went wrong !");
   }
